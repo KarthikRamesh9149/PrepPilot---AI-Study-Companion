838935a3303a758b0d2cbf096fd3989e23fa8f07
KarthikRamesh9149 <karthikramesh2012@gmail.com>
2026-02-24 13:24:12 +0530

    Remove tests and smoke scripts from repository package

--- tests/test_cache_key_profile_model.py
+++
@@ -1,9 +0,0 @@
-﻿from exam_helper.utils.cache import cache_key
-
-
-def test_cache_key_profile_model():
-    k1 = cache_key(["v2", "module", "plan", "2026-02-24", "2026-03-01", "modelA", "{'hours':2}"])
-    k2 = cache_key(["v2", "module", "plan", "2026-02-24", "2026-03-01", "modelB", "{'hours':2}"])
-    k3 = cache_key(["v2", "module", "plan", "2026-02-24", "2026-03-01", "modelA", "{'hours':4}"])
-    assert k1 != k2
-    assert k1 != k3
